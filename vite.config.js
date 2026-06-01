8bcdba48b641bd048e5c46c2d8657d72fb078190
IEROGLIF <ieroglif.kerch@gmail.com>
2026-06-01 17:21:41 +0300

    build: finally

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/ai-project-my/',
   root: '.',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
   server: {
     port: 5173,
     open: false,
